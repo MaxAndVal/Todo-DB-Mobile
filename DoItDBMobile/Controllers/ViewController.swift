7a6e8bbb95239bb7da33d103fc891708227d1741
Zigoval <rouby.v@hotmail.com>
2019-04-01 15:53:16 +0200

    idem

--- DoItDBMobile/Controllers/ViewController.swift
+++ DoItDBMobile/Controllers/ViewController.swift
@@ -77,6 +77,20 @@ class ViewController: UIViewController {
         return tempTable
     }
     
+    func deleteRowByTitle(title: String?, index: Int){
+        let request = NSFetchRequest<TodoItem>(entityName: "TodoItem")
+        request.predicate = NSPredicate(format: "title = %@", title ?? "")
+        do {
+            let result = try self.context.fetch(request)
+            items.remove(at: index)
+            for item in result {
+                context.delete(item)
+            }
+        } catch let error as NSError {
+            print("error : \(error)")
+        }
+    }
+    
     //MARK:- Actions
     @IBAction func addItem(_ sender: UIBarButtonItem) {
         
@@ -92,15 +106,15 @@ class ViewController: UIViewController {
             newItem.title = newItemTitle
             newItem.category = "none"
             self.items.append(newItem)
+            let tempTable = self.tempTableByCat(category: "none")
             if self.isFiltered
             {
                 self.filteredItems.append(newItem)
                 self.tableView.insertRows(at: [IndexPath(item: self.filteredItems.count - 1, section: 0)], with: .automatic)
             }else{
-                var tempTable = self.tempTableByCat(category: "none")
                 self.tableView.insertRows(at: [IndexPath(item: tempTable.count - 1, section: 0)], with: .automatic)
             }
-            
+            self.tableView.reloadData()
             self.saveItems()
         }
         
@@ -202,35 +216,22 @@ extension ViewController : UITableViewDelegate, UITableViewDataSource {
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        var tempTable = tempTableByCat(category: categories[section].catName!)
-        
+        let tempTable = tempTableByCat(category: categories[section].catName!)
         return isFiltered ? filteredItems.count : tempTable.count
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return isFiltered ? filteredItems.count : categories.count
+        return isFiltered ? 1 : categories.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        var tempTable = [TodoItem]()
-        
-        let fetchRequest: NSFetchRequest<TodoItem> = NSFetchRequest<TodoItem>(entityName: "TodoItem")
-        fetchRequest.predicate = NSPredicate(format: "category contains[c] %@", categories[indexPath.section].catName ?? "none")
-        do {
-            let fetchedResults = try self.context.fetch(fetchRequest)
-            let results = fetchedResults as [NSManagedObject]
-            for item in results {
-                tempTable.append(item as! TodoItem)
-            }
-        } catch let error as NSError {
-            print("Could not fetch : \(error)")
-        }
+        var tempTable = tempTableByCat(category: categories[indexPath.section].catName!)
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cellidentifier") as! CheckItemTableViewCell
         
         let task = isFiltered ? filteredItems[indexPath.row] : tempTable[indexPath.row]
-        //if(task.category == titleF)
+        
         cell.cellTextField.text = task.title
         cell.checkmark.isHidden = !task.checkmark
         
@@ -242,27 +243,31 @@ extension ViewController : UITableViewDelegate, UITableViewDataSource {
             filteredItems[indexPath.row].checkmark = !filteredItems[indexPath.row].checkmark
             
         }else{
-            items[indexPath.row].checkmark = !items[indexPath.row].checkmark
-            
+            var tempTable = tempTableByCat(category: categories[indexPath.section].catName!)
+            tempTable[indexPath.row].checkmark = !tempTable[indexPath.row].checkmark
         }
-        tableView.reloadRows(at: [indexPath], with: .automatic)
         tableView.deselectRow(at: indexPath, animated: true)
+        tableView.reloadData()
         saveItems()
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         
+        
         if (editingStyle == .delete) {
             if isFiltered
             {
-                items = items.filter{$0 !== filteredItems[indexPath.item]}
-                filteredItems.remove(at: indexPath.item)
+                let index = items.firstIndex{$0 == filteredItems[indexPath.item]}
+                filteredItems.remove(at: indexPath.row)
+                deleteRowByTitle(title: items[index!].title, index: index!)
             }
             else{
-                //var tempTable = tempTableByCat(category: categories[indexPath.section].catName ?? "none" )
-                //let index = items.firstIndex{$0.category == tempTable[indexPath.row].category} ?? -1
+                var tempTable = tempTableByCat(category: categories[indexPath.section].catName!)
+                let index = items.firstIndex{$0 == tempTable[indexPath.row]} ?? -1
+                deleteRowByTitle(title: items[index].title, index : index)
             }
             tableView.deleteRows(at: [indexPath], with: .automatic)
+            tableView.reloadData()
             saveItems()
         }
     }
@@ -277,6 +282,7 @@ extension ViewController : UISearchBarDelegate {
         }else{
             filteredItems = []
             isFiltered = true
+            print("test", items)
             
             let fetchRequest: NSFetchRequest<TodoItem> = NSFetchRequest<TodoItem>(entityName: "TodoItem")
             fetchRequest.predicate = NSPredicate(format: "title contains[c] %@", searchText)
@@ -292,12 +298,11 @@ extension ViewController : UISearchBarDelegate {
                 print("Could not fetch : \(error)")
             }
             
-            //filteredItems = items.filter { $0.title!.lowercased().contains(searchBar.text!.lowercased()) }
-            
         }
     }
     
     func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        print("searchBarTextDidEndEditing")
         if(searchBar.text?.count==0){
             isFiltered = false
             tableView.reloadData()
